d73f6065cac02596d05ea2a7782cbd21a9fbbb1c
Firat <firatberkcakar@gmail.com>
2019-08-01 12:23:09 +0200

    gitignore1

--- cmake-build-debug/CMakeFiles/tuw_collision_alarm_node.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for tuw_collision_alarm_node.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/firat/projects/catkin/mpn/ws01/src/tuw_collision_alarm
-# Build directory: /home/firat/projects/catkin/mpn/ws01/src/tuw_collision_alarm/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
